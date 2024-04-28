980928020a3a94a7d71311527dbf310df981006f
moidalam05 <moidalam005@hotmail.com>
2024-04-29 01:05:16 +0530

    Add updateUser functionality to user.controller.js and user.routes.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,6 @@
+import bcrypt from 'bcryptjs';
+import { v2 as cloudinary } from 'cloudinary';
+
 import User from '../models/user.model.js';
 import Notification from '../models/notification.model.js';
 
@@ -76,5 +79,91 @@ export const getSuggestedUsers = async (req, res) => {
 	} catch (error) {
 		console.error('Error in getSuggestedUsers', error.message);
 		res.status(500).json({ message: 'Internal Server Error' });
+	}
+};
+
+export const updateUser = async (req, res) => {
+	const {
+		fullName,
+		email,
+		username,
+		currentPassword,
+		newPassword,
+		bio,
+		link,
+	} = req.body;
+	let { profilePicture, coverPicture } = req.body;
+
+	const userId = req.user._id;
+
+	try {
+		let user = await User.findById(userId);
+		if (!user) return res.status(404).json({ message: 'User not found' });
+
+		if (
+			(!newPassword && currentPassword) ||
+			(!currentPassword && newPassword)
+		) {
+			return res
+				.status(400)
+				.json({
+					error: 'Please provide both current password and new password',
+				});
+		}
+
+		if (currentPassword && newPassword) {
+			const isMatch = await bcrypt.compare(currentPassword, user.password);
+			if (!isMatch)
+				return res.status(400).json({ error: 'Current password is incorrect' });
+			if (newPassword.length < 8) {
+				return res
+					.status(400)
+					.json({ error: 'Password must be at least 8 characters long' });
+			}
+
+			const salt = await bcrypt.genSalt(10);
+			user.password = await bcrypt.hash(newPassword, salt);
+		}
+
+		if (profilePicture) {
+			if (user.profilePicture) {
+				// https://res.cloudinary.com/dyfqon1v6/image/upload/v1712997552/zmxorcxexpdbh8r0bkjb.png
+				await cloudinary.uploader.destroy(
+					user.profilePicture.split('/').pop().split('.')[0]
+				);
+			}
+
+			const uploadedResponse = await cloudinary.uploader.upload(profilePicture);
+			profilePicture = uploadedResponse.secure_url;
+		}
+
+		if (coverPicture) {
+			if (user.coverPicture) {
+				await cloudinary.uploader.destroy(
+					user.coverPicture.split('/').pop().split('.')[0]
+				);
+			}
+
+			const uploadedResponse = await cloudinary.uploader.upload(coverPicture);
+			coverPicture = uploadedResponse.secure_url;
+		}
+
+		user.fullName = fullName || user.fullName;
+		user.email = email || user.email;
+		user.username = username || user.username;
+		user.bio = bio || user.bio;
+		user.link = link || user.link;
+		user.profilePicture = profilePicture || user.profilePicture;
+		user.coverPicture = coverPicture || user.coverPicture;
+
+		user = await user.save();
+
+		// password should be null in response
+		user.password = null;
+
+		return res.status(200).json(user);
+	} catch (error) {
+		console.log('Error in updateUser: ', error.message);
+		res.status(500).json({ error: error.message });
 	}
 };
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import { protectedRoute } from '../middlewares/protectRoute.js';
-import { followUnfollowUser, getSuggestedUsers, getUserProfile } from '../controllers/user.controller.js';
+import {
+	followUnfollowUser,
+	getSuggestedUsers,
+	getUserProfile,
+	updateUser,
+} from '../controllers/user.controller.js';
 const router = express.Router();
 
 router.get('/profile/:username', protectedRoute, getUserProfile);
 router.get('/suggested', protectedRoute, getSuggestedUsers);
 router.post('/follow/:id', protectedRoute, followUnfollowUser);
-// router.post('/update', protectedRoute, updateUserProfile);
+router.post('/update', protectedRoute, updateUser);
 
 export default router;
