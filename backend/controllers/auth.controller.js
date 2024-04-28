db8bf50a7e10842e59078205b6ed55df3c8d47c2
moidalam05 <moidalam005@hotmail.com>
2024-04-28 14:04:31 +0530

    add logout functionality and handle errors in auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -100,7 +100,11 @@ export const login = async (req, res) => {
 };
 
 export const logout = async (req, res) => {
-	res.json({
-		data: 'You hit the logout endpoint',
-	});
+	try {
+		res.cookie('jwt', '', { maxAge: 0 });
+		res.status(200).json({ message: 'User logged out successfully' });
+	} catch (error) {
+		console.error('Error in logout', error);
+		res.status(500).json({ error: 'Internal server error' });
+	}
 };
