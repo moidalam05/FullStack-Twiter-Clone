33fbf4d01b25807a5639261f5dace9b3c2c787c7
moidalam05 <moidalam005@hotmail.com>
2024-04-29 01:06:10 +0530

    Fix password validation in updateUser function

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -104,11 +104,9 @@ export const updateUser = async (req, res) => {
 			(!newPassword && currentPassword) ||
 			(!currentPassword && newPassword)
 		) {
-			return res
-				.status(400)
-				.json({
-					error: 'Please provide both current password and new password',
-				});
+			return res.status(400).json({
+				error: 'Please provide both current password and new password',
+			});
 		}
 
 		if (currentPassword && newPassword) {
