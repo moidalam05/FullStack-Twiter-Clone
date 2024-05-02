d39bdec3f150a05044be59d0d697b0ae0a5d54a7
moidalam05 <moidalam005@hotmail.com>
2024-05-02 16:09:15 +0530

    Add deletePost functionality to post.controller.js and post.routes.js

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { protectedRoute } from '../middlewares/protectRoute.js';
-import { createPost } from '../controllers/post.controller.js';
+import { createPost, deletePost } from '../controllers/post.controller.js';
 const router = express.Router();
 
 router.post('/create', protectedRoute, createPost);
 // router.post('/like/:id', protectedRoute, likeUnlikePost);
 // router.post('/comment/:id', protectedRoute, commentOnPost);
-// router.delete('/', protectedRoute, deletePost);
+router.delete('/:id', protectedRoute, deletePost);
 
 export default router;
