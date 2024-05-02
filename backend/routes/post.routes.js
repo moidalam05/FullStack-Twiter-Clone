696b35ae664c7f3d43a97ada3a294c5582024a6d
moidalam05 <moidalam005@hotmail.com>
2024-05-02 17:42:27 +0530

    Add functionality to like and unlike posts, and retrieve liked posts

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -5,11 +5,13 @@ import {
 	createPost,
 	deletePost,
 	getAllPosts,
+	getLikedPosts,
 	likeUnlikePost,
 } from '../controllers/post.controller.js';
 const router = express.Router();
 
 router.get('/all', protectedRoute, getAllPosts);
+router.get('/likes/:id', protectedRoute, getLikedPosts);
 router.post('/create', protectedRoute, createPost);
 router.post('/like/:id', protectedRoute, likeUnlikePost);
 router.post('/comment/:id', protectedRoute, commentOnPost);
