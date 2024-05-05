78d7b32ce807d1df4a6029a8102a0a89ff5c1512
moidalam05 <moidalam005@hotmail.com>
2024-05-05 17:35:10 +0530

    UI Design complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,25 @@
+import { Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/auth/login/LoginPage';
+import SignUpPage from './pages/auth/signup/SignUpPage';
+import HomePage from './pages/home/HomePage';
+import Sidebar from './components/common/Sidebar';
+import RightPanel from './components/common/RightPanel';
+import NotificationPage from './pages/notification/NotificationPage';
+import ProfilePage from './pages/profile/ProfilePage';
+
 function App() {
 	return (
-		<>
-			<button className='btn btn-primary'>Primary</button>
-		</>
+		<div className='flex max-w-6xl mx-auto'>
+			<Sidebar />
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/signup' element={<SignUpPage />} />
+				<Route path='/login' element={<LoginPage />} />
+				<Route path='/notifications' element={<NotificationPage />} />
+				<Route path='/profile/:username' element={<ProfilePage />} />
+			</Routes>
+			<RightPanel />
+		</div>
 	);
 }
 
