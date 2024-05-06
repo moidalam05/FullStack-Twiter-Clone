fb064bf3b22281ed926108204b1bf5a3d5ce1363
moidalam05 <moidalam005@hotmail.com>
2024-05-06 12:53:15 +0530

    signup functionality developed with ui

--- frontend/src/pages/auth/signup/SignUpPage.jsx
+++ frontend/src/pages/auth/signup/SignUpPage.jsx
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
-
 import XSvg from '../../../components/svgs/X';
-
 import { MdOutlineMail } from 'react-icons/md';
 import { FaUser } from 'react-icons/fa';
 import { MdPassword } from 'react-icons/md';
 import { MdDriveFileRenameOutline } from 'react-icons/md';
+import { useMutation } from '@tanstack/react-query';
+import { toast } from 'react-hot-toast';
 
 const SignUpPage = () => {
 	const [formData, setFormData] = useState({
@@ -16,16 +16,39 @@ const SignUpPage = () => {
 		password: '',
 	});
 
+	const { mutate, isError, isPending, error } = useMutation({
+		mutationFn: async ({ email, username, fullName, password }) => {
+			try {
+				const res = await fetch('/api/auth/signup', {
+					method: 'POST',
+					headers: {
+						'content-type': 'application/json',
+					},
+					body: JSON.stringify({ email, username, fullName, password }),
+				});
+				const data = await res.json();
+				if (!res.ok) throw new Error(data.error);
+				if (data.error) throw new Error(data.error);
+				console.log(data);
+				return data;
+			} catch (error) {
+				console.error(error);
+				throw error;
+			}
+		},
+		onSuccess: () => {
+			toast.success('User registered successfully');
+		},
+	});
+
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log(formData);
+		mutate(formData);
 	};
 
 	const handleInputChange = (e) => {
 		setFormData({ ...formData, [e.target.name]: e.target.value });
 	};
-
-	const isError = false;
 
 	return (
 		<div className='max-w-screen-xl mx-auto flex h-screen px-10'>
@@ -86,9 +109,9 @@ const SignUpPage = () => {
 						/>
 					</label>
 					<button className='btn rounded-full btn-primary text-white'>
-						Sign up
+						{isPending ? 'Loading...' : 'Sign Up'}
 					</button>
-					{isError && <p className='text-red-500'>Something went wrong</p>}
+					{isError && <p className='text-red-500'>{error.message}</p>}
 				</form>
 				<div className='flex flex-col lg:w-2/3 gap-2 mt-4'>
 					<p className='text-white text-lg'>Already have an account?</p>
