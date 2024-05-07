0ce11a58fc1fdba415d5fb3edbac0f03aafba1f1
moidalam05 <moidalam005@hotmail.com>
2024-05-07 23:52:01 +0530

    Update NotificationPage to use react-query for fetching notifications and add delete functionality

--- frontend/src/pages/notification/NotificationPage.jsx
+++ frontend/src/pages/notification/NotificationPage.jsx
@@ -1,37 +1,47 @@
 import { Link } from 'react-router-dom';
 import LoadingSpinner from '../../components/common/LoadingSpinner';
-
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { IoSettingsOutline } from 'react-icons/io5';
 import { FaUser } from 'react-icons/fa';
 import { FaHeart } from 'react-icons/fa6';
+import { toast } from 'react-hot-toast';
 
 const NotificationPage = () => {
-	const isLoading = false;
-	const notifications = [
-		{
-			_id: '1',
-			from: {
-				_id: '1',
-				username: 'johndoe',
-				profileImg: '/avatars/boy2.png',
-			},
-			type: 'follow',
+	const queryClient = useQueryClient();
+	const { data: notifications, isLoading } = useQuery({
+		queryKey: ['notifications'],
+		queryFn: async () => {
+			try {
+				const res = await fetch('/api/notification');
+				const data = await res.json();
+				if (!res.ok) throw new Error(data.error || 'Something went wrong');
+				return data;
+			} catch (error) {
+				throw new Error(error);
+			}
 		},
-		{
-			_id: '2',
-			from: {
-				_id: '2',
-				username: 'janedoe',
-				profileImg: '/avatars/girl1.png',
-			},
-			type: 'like',
+	});
+
+	const { mutate: deleteNotifications } = useMutation({
+		mutationFn: async () => {
+			try {
+				const res = await fetch('/api/notification', {
+					method: 'DELETE',
+				});
+				const data = await res.json();
+				if (!res.ok) throw new Error(data.error || 'Something went wrong');
+				return data;
+			} catch (error) {
+				throw new Error(error);
+			}
 		},
-	];
-
-	const deleteNotifications = () => {
-		alert('All notifications deleted');
-	};
-
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ['notifications'] });
+		},
+		onError: (error) => {
+			toast.error(error.message);
+		},
+	});
 	return (
 		<>
 			<div className='flex-[4_4_0] border-l border-r border-gray-700 min-h-screen'>
