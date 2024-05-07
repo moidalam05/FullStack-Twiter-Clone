417bdfedc4e976476a592b8920aad5e873445367
moidalam05 <moidalam005@hotmail.com>
2024-05-07 16:14:25 +0530

    Update express.json() to include a limit for parsing JSON data

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(express.json()); // to parse json data
+app.use(express.json({ limit: '5mb' })); // to parse json data
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
