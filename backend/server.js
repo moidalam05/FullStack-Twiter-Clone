60cb9a15f86dc1356bcad4c40e6b54a95567ce46
moidalam05 <moidalam005@hotmail.com>
2024-05-08 23:27:07 +0530

    Update server.js and package.json

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -17,6 +18,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: '5mb' })); // to parse json data
 app.use(express.urlencoded({ extended: true }));
@@ -26,6 +28,14 @@ app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/notification', notificationRoutes);
+
+if (process.env.NODE_ENV === 'production') { 
+	app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+	});
+};
 
 app.listen(PORT, () => {
 	console.log(`server is running on port ${PORT}`);
